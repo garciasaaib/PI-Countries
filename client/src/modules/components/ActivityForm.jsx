b8c71578368ee2e4c7deb4ee87130282c77f1979
adranuz <garciasaaib@gmail.com>
2021-11-22 18:18:08 -0700

    data

--- client/src/modules/components/ActivityForm.jsx
+++ client/src/modules/components/ActivityForm.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const ActivityForm = () => {
   const [inputs, setInputs] = useState({
     name: '',
-    dificulty: 1,
+    difficulty: 1,
     duration: 1,
   })
 
