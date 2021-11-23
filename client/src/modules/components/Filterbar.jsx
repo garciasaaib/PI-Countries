4a8d7ac3bf2d4e7a0b9f9fe85079b85dafaae78c
Adrian Garcia <garciasaaib@gmail.com>
2021-11-22 18:15:09 -0700

    pasando a desktop

--- client/src/modules/components/Filterbar.jsx
+++ client/src/modules/components/Filterbar.jsx
@@ -2,8 +2,43 @@ const Filterbar = () => {
   return (
     <div>
       <form>
-        <input type="text"/>
+        <input 
+          type="text"
+          placeholder="Country..."
+        />
       </form>
+
+
+      <div>
+        <label htmlFor="">Continent</label>
+        <select name="continent" id="">
+          <option>All</option>
+          <option>Africa</option>
+          <option>Asia</option>
+          <option>America</option>
+          <option>Europe</option>
+          <option>Oceania</option>
+          <option>Polar</option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="">Order by Country</label>
+        <select name="" id="">
+          <option></option>
+          <option>Asc</option>
+          <option>Des</option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="">Order by Population</label>
+        <select name="" id="">
+          <option></option>
+          <option>Asc</option>
+          <option>Des</option>
+        </select>
+      </div>
     </div>
   );
 }
